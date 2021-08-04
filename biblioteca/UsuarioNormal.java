1e94ca0e1d50f10e1644e9932becb870746d031b
GuiiCorreia29581 <78569072+GuiiCorreia29581@users.noreply.github.com>
2021-08-04 13:00:32 -0300

    Update UsuarioNormal.java

--- biblioteca/UsuarioNormal.java
+++ biblioteca/UsuarioNormal.java
@@ -1,7 +1,11 @@
 
-public class UsuarioComum extends Usuario{
+public class UsuarioNormal extends Usuario{
     private int limiteEmprestimo = 5;
     public UsuarioComum(String nome, String cpf, String email, String telefone){
+        //private String nome;
+        //private String cpf;
+        //private String email;
+        //private String telefone;
         super(nome, cpf, email, telefone);
     }
     public void setLimite(int limiteEmprestimo) {
